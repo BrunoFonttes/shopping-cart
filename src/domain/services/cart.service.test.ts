0f53afdfe65dfa067c3576dd7047392b08c6a632
Bruno Fontes <brunobfonttes@gmail.com>
2023-08-10 16:58:53 -0300

    test(cart service): adds cart service tests skeleton

---
+++ src/domain/services/cart.service.test.ts
@@ -0,0 +1,19 @@
+describe("tests cart service", () => {
+	describe("tests addOrUpdateItem", () => {
+		it("should return failure when item doesnt exist", () => {});
+		it("should return failure when cartItem has validation errors", () => {});
+		it("should return failure when its not possible to create/update cart", () => {});
+		it("should return success", () => {});
+	});
+
+	describe("tests removeItem", () => {
+		it("should return failure when item doesnt exist", () => {});
+		it("should return failure when its not possible to create/update cart", () => {});
+		it("should return success", () => {});
+	});
+
+	describe("tests getCart", () => {
+		it("should return failure when its not possible to get cart", () => {});
+		it("should return success", () => {});
+	});
+});
