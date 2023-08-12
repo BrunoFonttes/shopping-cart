fab8847de58ccc02f1f35a4c7e1ca3624e095999
Bruno Fontes <brunobfonttes@gmail.com>
2023-08-12 12:45:22 -0300

    feat(cart repository port): removes create method

--- src/domain/ports/repositories/cart.repository.port.ts
+++ src/domain/ports/repositories/cart.repository.port.ts
@@ -1,6 +1,6 @@
 import { Either } from "../../../core/either";
 import { Cart } from "../../entities/cart.entity";
-import { CartItem } from "../../entities/cartItem.valueObject";
+import { CartItem } from "../../entities/cartItem.entity";
 import { Item } from "../../entities/item.entity";
 
 /**
@@ -9,11 +9,7 @@ import { Item } from "../../entities/item.entity";
  *  and if it fails, decide in the place of the code the error happens to recover or not
  */
 export interface CartRepositoryPort {
-	create: (userId: number, cart: Cart) => Promise<Either<Error, Cart>>;
 	getByUserId: (userId: number) => Promise<Either<Error, Cart>>;
-	addOrUpdateItem: (
-		userId: number,
-		cartItem: CartItem,
-	) => Promise<Either<Error, Cart>>;
+	addOrUpdateItem: (userId: number, cartItem: CartItem) => Promise<Either<Error, Cart>>;
 	removeItem: (userId: number, item: Item) => Promise<Either<Error, Cart>>;
 }
