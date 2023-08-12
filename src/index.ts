f50421e023bdbdbd209e3f7eaa2867844715a004
Bruno Fontes <brunobfonttes@gmail.com>
2023-08-12 13:17:47 -0300

    feat(index): adds routes and middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import express from "express";
 
-const PORT = 3000;
+import rTracer from "cls-rtracer";
+import morgan from "morgan";
+import { envs } from "./config/env.factory";
+import { Container } from "./container";
+import { logger } from "./infra/logger";
+import { authHandler, errorHandler } from "./presentation/middlewares";
 
-const app = express();
+const { controllers } = new Container();
+
+export const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+morgan.token("request-id", function (_req, _res) {
+	return rTracer.id() as string;
+});
+
+app.use(rTracer.expressMiddleware());
+app.use(
+	morgan(
+		`[${envs.serviceName}] :method :url :status :res[content-length] - :response-time ms [:request-id]`,
+	),
+);
 
 app.use(express.json({ limit: "50kb" }));
 
@@ -10,4 +29,26 @@ app.get("/health", (_req, res) => {
 	return res.status(200).send("ok");
 });
 
-app.listen(PORT, () => {});
+app.use(authHandler);
+
+app.post("/cart", controllers.cart.addOrUpdateItems);
+app.delete("/cart/item/:itemId", controllers.cart.removeItem);
+app.get("/cart", controllers.cart.get);
+
+app.use(errorHandler);
+
+export const server = app.listen(envs.port, () => {
+	logger.info(`server listening at port ${envs.port}`);
+});
+
+/**
+ * Simple graceful shutdown
+ */
+process.on("SIGTERM", () => {
+	logger.info("SIGTERM signal received.");
+	logger.info("closing http server.");
+	server.close(() => {
+		logger.info("http server closed.");
+		process.exit(0);
+	});
+});
