b06d190e568d872c26fdeab7eb57533d3ec9badc
Bruno Fontes <brunobfonttes@gmail.com>
2023-08-12 12:46:06 -0300

    feat(service ports): adds cart service port

--- src/domain/ports/services/cart.service.port.ts
+++ src/domain/ports/services/cart.service.port.ts
@@ -2,11 +2,7 @@ import { Either } from "../../../core/either";
 import { Cart } from "../../entities/cart.entity";
 
 export interface CartServicePort {
-	addOrUpdateItem(
-		userId: number,
-		itemId: number,
-		amount: number,
-	): Promise<Either<Error, void>>;
+	addOrUpdateItem(userId: number, itemId: number, amount: number): Promise<Either<Error, void>>;
 
 	removeItem: (userId: number, itemId: number) => Promise<Either<Error, void>>;
 
